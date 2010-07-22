50c59d3bdc925c051f22fe1e66b083a3af397e45
Jax Law <jax@rockmelt.com>
2010-07-21 18:29:49 -0700

    added -A

--- fairy-core/src/main/java/com/mewmew/fairy/v1/book/Jmx.java
+++ fairy-core/src/main/java/com/mewmew/fairy/v1/book/Jmx.java
@@ -18,6 +18,7 @@
 */
 package com.mewmew.fairy.v1.book;
 
+import com.google.common.collect.ImmutableMap;
 import com.mewmew.fairy.v1.cli.Param;
 import com.mewmew.fairy.v1.json.JsonOutput;
 import com.mewmew.fairy.v1.json.OutputFormat;
@@ -35,66 +36,97 @@ import java.util.*;
 @Help(desc = "JMX cli interface")
 public class Jmx extends Spell {
 
-    @Param String host ;
-    @Param int port ;
-    @Param String objectName = null;
-    @Param(option="O") private OutputFormat outputFormat;
-    @Param private String[] columnOrder;
+    @Param(option = "H")
+    String host;
+    @Param
+    int port;
+    @Param
+    String objectName = null;
+    @Param(option = "O", defaultValue = "PRETTY")
+    private OutputFormat outputFormat;
+    @Param
+    private String[] columnOrder;
+    @Param(option = "A")
+    String[] attributes;
 
 
     // TODO : add -A -O and -V and make host a list or from standard in
 
     @Override
-    public void cast()
-    {
-        try
-        {
-            describe(host, port, objectName == null ? null : new ObjectName(objectName), JsonOutput.createOutput(getOutputStream(), outputFormat, columnOrder));
-        } catch (Exception e)
-        {
-            e.printStackTrace();
+    public void cast() {
+        if (attributes != null && objectName != null) {
+            try {
+                getAttributes(host, port, objectName, attributes, JsonOutput.createOutput(getOutputStream(), outputFormat, columnOrder));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                describe(host, port, objectName == null ? null : new ObjectName(objectName), JsonOutput.createOutput(getOutputStream(), outputFormat, columnOrder));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    
-    private void describe(String host, int port, ObjectName objName, Output<Map<String, Object>> output)
-    {
-        try {
-            List<Map<String, Object>> array = new ArrayList<Map<String, Object>>();
-            Map<String, Object> container = new HashMap<String, Object>();
-            for (ObjectName desc : new TreeSet<ObjectName>(getMBeanServerConnection(host, port).queryNames(objName, null))) {
-                Map<String, Object> json = new HashMap<String, Object>();
-                MBeanServerConnection beanServerConnection = getMBeanServerConnection(host, port);
-                MBeanInfo info = beanServerConnection.getMBeanInfo(desc);
-                json.put("objectName", desc.toString());
-                List<Map<String, Object>> attrs = new ArrayList<Map<String, Object>>();
-                for (MBeanAttributeInfo attr : info.getAttributes()) {
-                    Map<String, Object> attrJson = new HashMap<String, Object>();
-                    attrJson.put("type", attr.getType());
-                    attrJson.put("description", attr.getDescription());
-                    attrJson.put("name", attr.getName());
-                    attrJson.put("isReadable", attr.isReadable());
-                    attrJson.put("isWritable", attr.isWritable());
-                    attrs.add(attrJson);
+    private void getAttributes(final String host, final int port, final String objectName, final String[] attributes, final Output<Map<String, Object>> output) {
+        withMBeanServerConnection(host, port, new MBeanServerCallback() {
+            public void withConnection(MBeanServerConnection conn) throws Exception {
+                Map<String, Object> container = new HashMap<String, Object>();
+                List<Map<String, Object>> array = new ArrayList<Map<String, Object>>();
+                AttributeList attrs = conn.getAttributes(new ObjectName(objectName), attributes);
+                for (int i = 0; i < attrs.size(); i++) {
+                    Attribute attr = (Attribute) attrs.get(i);
+                    array.add(ImmutableMap.of("name", attr.getName(), "value", attr.getValue()));
                 }
-                json.put("attributes", attrs);
-                array.add(json);
+                container.put("host", host);
+                container.put("jmxPort", port);
+                container.put("objectName", objectName);
+                container.put("attributes", array);
+                output.output(container);
             }
-            container.put("host", host);
-            container.put("jmxPort", port);
-            container.put("mbeans", array) ;
-            output.output(container);
+        });
+    }
+
+
+    private void describe(final String host, final int port, final ObjectName objName, final Output<Map<String, Object>> output) {
+        withMBeanServerConnection(host, port, new MBeanServerCallback() {
+            public void withConnection(MBeanServerConnection conn) throws Exception {
+                List<Map<String, Object>> array = new ArrayList<Map<String, Object>>();
+                Map<String, Object> container = new HashMap<String, Object>();
+                for (ObjectName desc : new TreeSet<ObjectName>(conn.queryNames(objName, null))) {
+                    Map<String, Object> json = new HashMap<String, Object>();
+                    MBeanInfo info = conn.getMBeanInfo(desc);
+                    json.put("objectName", desc.toString());
+                    json.put("info", info);
+                    array.add(json);
+                }
+                container.put("host", host);
+                container.put("jmxPort", port);
+                container.put("mbeans", array);
+                output.output(container);
+            }
+        });
+    }
+
+    private void withMBeanServerConnection(String host, int port, MBeanServerCallback callback) {
+        JMXConnector connector = null;
+        try {
+            connector = JMXConnectorFactory.connect(new JMXServiceURL(String.format("service:jmx:rmi:///jndi/rmi://%s:%d/jmxrmi", host, port)));
+            callback.withConnection(connector.getMBeanServerConnection());
         }
         catch (Exception e) {
             e.printStackTrace();
         }
+        finally {
+            try {
+                connector.close();
+            } catch (Exception e) {
+            }
+        }
     }
 
-    private MBeanServerConnection getMBeanServerConnection(String host, int port) throws IOException
-    {
-        String url = String.format("service:jmx:rmi:///jndi/rmi://%s:%d/jmxrmi", host, port);
-        JMXServiceURL urlObj = new JMXServiceURL(url);
-        JMXConnector jmxConn = JMXConnectorFactory.connect(urlObj);
-        return jmxConn.getMBeanServerConnection();
+    interface MBeanServerCallback {
+        void withConnection(MBeanServerConnection conn) throws Exception;
     }
 }
